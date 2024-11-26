caf27b599fb42e3e49e08235dd4ca8422f9bd8ba
Kaveesha <kaveeshewaduge111@gmail.com>
2024-11-26 19:02:02 +0530

    Player move timers

--- Space_Shooter/Space_Shooter/Form1.cs
+++ Space_Shooter/Space_Shooter/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,7 +15,9 @@ namespace Space_Shooter
     {
         PictureBox[] stars;
         int backgroundspeed;
+        int playerSpeed;
         Random rnd;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +26,7 @@ namespace Space_Shooter
         private void Form1_Load(object sender, EventArgs e)
         {
             backgroundspeed = 4;
+            playerSpeed = 4;
             stars = new PictureBox[10];
             rnd = new Random();
 
@@ -46,7 +50,7 @@ namespace Space_Shooter
         }
         private void MoveBgTimer_Tick(object sender, EventArgs e)
         {
-            for(int i = 0;i < stars.Length/2;i++)
+            for (int i = 0; i < stars.Length/2; i++)
             {
                 stars[i].Top += backgroundspeed;
                 if (stars[i].Top >= this.Height)
@@ -54,14 +58,73 @@ namespace Space_Shooter
                     stars[i].Top = -stars[i].Height;
                 }
             }
-            for (int i = stars.Length / 2; i < stars.Length; i++)
+            for (int i = stars.Length/2; i < stars.Length; i++)
             {
-                stars[i].Top += backgroundspeed-2;
+                stars[i].Top += backgroundspeed - 2;
                 if (stars[i].Top >= this.Height)
                 {
                     stars[i].Top = -stars[i].Height;
                 }
             }
+        }
+
+        private void LeftMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if(Player.Left > 10)
+            {
+                Player.Left -= playerSpeed;
+            }
+        }
+
+        private void RightMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Right < 580)
+            {
+                Player.Left += playerSpeed;
+            }
+        }
+
+        private void DownMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top < 400)
+            {
+                Player.Top += playerSpeed;
+            }
+        }
+        private void UpMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top > 10)
+            {
+                Player.Top -= playerSpeed;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                RightMoveTimer.Start();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                LeftMoveTimer.Start();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                DownMoveTimer.Start();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                UpMoveTimer.Start();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            RightMoveTimer.Stop();
+            LeftMoveTimer.Stop();
+            DownMoveTimer.Stop();
+            UpMoveTimer.Stop();
         }
     }
 }
