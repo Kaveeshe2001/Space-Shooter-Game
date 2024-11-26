3e0163359c260e0f34dc8761a72f1e44f716ba83
Kaveesha <kaveeshewaduge111@gmail.com>
2024-11-26 16:38:06 +0530

    Initial Create

---
+++ Space_Shooter/Space_Shooter/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Shooter
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] stars;
+        int backgroundspeed;
+        Random rnd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundspeed = 4;
+            stars = new PictureBox[10];
+            rnd = new Random();
+        }
+    }
+}
